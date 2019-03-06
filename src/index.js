9b49cd8353fc56bbb39851e1eb90d73c631446c9
Alex Read <alexread730@gmail.com>
2019-03-05 19:55:24 -0700

    remove commented code and add tile list component to root

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import LocationTile from './components/LocationTile'
+import TileList from './components/TileList/TileList';
 
 //Create a react component
 class App extends Component {
@@ -55,45 +55,10 @@ class App extends Component {
   }
   
   render() {
-    // const locations = [
-    //   {
-    //     id: 1,
-    //     userId: 1,
-    //     name: "Thailand/Bali",
-    //     description: 'New Years trip to Thialand and Bali with the Tenners!',
-    //     notes: '',
-    //     startDate: new Date(2018,12,25),
-    //     endDate: new Date(2019,1,4),
-    //     photos: [],
-    //     positions: []
-    //   },
-    //   {
-    //     id: 2,
-    //     userId: 1,
-    //     name: "Christmas 2018",
-    //     description: 'Christmas with the Reads',
-    //     notes: '',
-    //     startDate: new Date(2018,12,19),
-    //     endDate: new Date(2018,12,25),
-    //     photos: [],
-    //     positions: []
-    //   },
-    //   {
-    //     id: 3,
-    //     userId: 1,
-    //     name: "Utah",
-    //     description: 'Alex and Tenner trip in the desert.',
-    //     notes: '',
-    //     startDate: new Date(2018,8,21),
-    //     endDate: new Date(2018,8,24),
-    //     photos: [],
-    //     positions: []
-    //   }
-    // ]
     
     return (
-      <div>
-        <LocationTile locations={this.state.locations} />
+      <div className="tile-list">
+        <TileList locations={this.state.locations} />
       </div>
     )
   };
